4865cdadba13ac52bdc76afd82ba8e80b04261ca
Rishabh Sharma <78745855+Rishabh5752@users.noreply.github.com>
2025-06-14 15:53:42 +0530

    done with qwen

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utils.logger import setup_logger
 from utils.config import CONFIG
 # from data_collection_pipeline.data_collection_main import run_data_collection_pipeline
 # from rag_pipeline.stage_00_clean_data_paths import run_clean_data_paths
-from rag_pipeline.stage_01_populate_db import run_populate_db
+# from rag_pipeline.stage_01_populate_db import run_populate_db
 from rag_pipeline.stage_02_query_data import run_query_rag
 
 # configurations
@@ -23,7 +23,7 @@ def main():
     
     # Create CLI.
     parser = argparse.ArgumentParser(description="MAIN WORKFLOW")
-    parser.add_argument("--reset", action="store_true", help="Reset Chroma DB before population")
+    # parser.add_argument("--reset", action="store_true", help="Reset Chroma DB before population")
     # parser.add_argument("query_text", type=str, help="The query text.")
     args = parser.parse_args()
     
@@ -31,7 +31,7 @@ def main():
     # try:
     #     logger.info(" ")
     #     logger.info("////--//--//----STARTING [PIPELINE 01] DATA COLLECTION----//--//--////")
-    #     # run_data_collection_pipeline()
+    #     run_data_collection_pipeline()
     #     logger.info("Already Done. Skipping...")
     #     logger.info("////--//--//----FINISHED [PIPELINE 01] DATA COLLECTION----//--//--////")
     #     logger.info(" ")
@@ -47,7 +47,7 @@ def main():
         # try:
         #     logger.info(" ")
         #     logger.info("----------STARTING [STAGE 00] CLEAN DATA PATHS----------")
-        #     # run_clean_data_paths()
+        #     run_clean_data_paths()
         #     logger.info("Already Done. Skipping...")
         #     logger.info("----------FINISHED [STAGE 00] CLEAN DATA PATHS----------")
         #     logger.info(" ")
@@ -56,17 +56,17 @@ def main():
         #     logger.debug(traceback.format_exc())
         #     return
         
-        try:
-            logger.info(" ")
-            logger.info("----------STARTING [STAGE 01] POPULATE DB----------")
-            run_populate_db(args.reset)
-            # logger.info("Already Done. Skipping...")
-            logger.info("----------FINISHED [STAGE 01] POPULATE DB----------")
-            logger.info(" ")
-        except Exception as e:
-            logger.error(f"ERROR RUNNING [STAGE 01] POPULATE DB: {e}")
-            logger.debug(traceback.format_exc())
-            return
+        # try:
+        #     logger.info(" ")
+        #     logger.info("----------STARTING [STAGE 01] POPULATE DB----------")
+        #     run_populate_db(args.reset)
+        #     # logger.info("Already Done. Skipping...")
+        #     logger.info("----------FINISHED [STAGE 01] POPULATE DB----------")
+        #     logger.info(" ")
+        # except Exception as e:
+        #     logger.error(f"ERROR RUNNING [STAGE 01] POPULATE DB: {e}")
+        #     logger.debug(traceback.format_exc())
+        #     return
         
         try:
             logger.info(" ")
@@ -91,3 +91,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
